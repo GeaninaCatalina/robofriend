9ed888b6f342f49cba6b0b5b77308032df2acb92
Geanina Catalina Masgras <masgras.geanina@hotmail.com>
2020-02-29 20:07:23 +0100

    changes in style

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Card = (props) => { 
   const {name, email, id} = props;
-  
+
   return (
-    <div className='tc bg-light-pink dib br3 pa3 ma2 grow bw2 shadow-5'>
+    <div className='tc bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img alt='player1' src={`https://robohash.org/${id}$`}/> 
       <div>
         <h2>{name}</h2>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = () => {
   return (
     <div className = 'pa2'>
     <input 
-          className = 'pa3 ba b--green bg-lightest-blue'
+          className = 'pa3 ba b--green bg-lightest-blue shadow-5'
           type='search' 
           placeholder = 'Search RoboPlayers'
     />
