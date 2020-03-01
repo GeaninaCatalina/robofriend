3fba9749568c236a507051227a57d91baba65fc5
Geanina <masgras.geanina@hotmail.com>
2020-03-01 22:52:29 +0100

    searchbox functional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react'; 
+import React,{ Component } from 'react'; 
 import CardList from './CardList'; 
 import {robots} from './robots';
 import SearchBox from './SearchBox'; 
+//import { Component } from 'react';
 
-  return (
-    <div className = 'tc'> 
-      <h1>RoboPlayers</h1>
-      <SearchBox />
-      <CardList robots = {robots} />
-    </div>
-  )
+class App  extends Component { 
+  constructor () {
+    super()
+    this.state = {
+      robots : robots, 
+      searchField : ''
+    }
+  }
+
+  onSearch = (event) => {
+    this.setState({ searchField: event.targ.value});
+   
+  }
+
+  render () {
+    const filteredRobots = this.state.robots.filter(robots => {
+    return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    }
+    ) 
+
+    return (
+      <div className = 'tc'> 
+        <h1>RoboPlayers</h1>
+        <SearchBox SearchChange = {this.onSearchChange} />
+        <CardList robots = {filteredRobots} />
+      </div>
+    );
+  }
+
+
+
+
 } 
 
 export default App; 
